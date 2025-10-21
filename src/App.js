c40edaded7b553cdc17aefbfa70247c900661852
Subodh <subodhlst@gmail.com>
2025-10-21 12:24:19 +0530

    add fake Api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <Header />
